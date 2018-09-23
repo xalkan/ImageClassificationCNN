c347bca5a9aaa56287a31c4bed919133774099fd
xalkan <shahidalyk@gmail.com>
2018-09-24 01:04:06 +0500

    image augmentation using keras preprocessing

--- cnn.py
+++ cnn.py
@@ -34,3 +34,21 @@ classifier.add(Dense(units = 1, activation = 'sigmoid'))
 # compiling the nn
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
+
+# 2 - Fitting the cnn to the image classifier after image augmentation
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set', target_size=(64, 64), batch_size=32, class_mode='binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set', target_size=(64, 64), batch_size=32, class_mode='binary')
+
+classifier.fit_generator(training_set, steps_per_epoch=8000, epochs=1, validation_data=test_set, validation_steps=2000)
+
