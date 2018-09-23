5de92b2de13c570e9773cf668c937a4848c778f5
xalkan <shahidalyk@gmail.com>
2018-09-24 01:09:24 +0500

    improved accuracy by adding another convolutional layer with max pooling

--- cnn.py
+++ cnn.py
@@ -23,6 +23,10 @@ classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'
 # adding pooling layer
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
+# adding another convolutional layer with max pooling
+classifier.add(Conv2D(32, (3, 3), activation = 'relu' ))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
 # flattening the input image
 classifier.add(Flatten())
 
